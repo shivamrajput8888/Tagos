a8091298f0223e0755920ca810c7c2824e198084
shivamrajput8888 <shivacer8888@gmail.com>
2026-07-09 16:02:10 +0530

    Refactor predict_tags call and update run configuration
    Refactor predict_tags call for better readability and update app.run parameters to use environment variable for port.

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
+import os
+
 from flask import Flask, render_template, request
 from predict import predict_tags
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET", "POST"])
 def home():
-
     predictions = None
     title = ""
     content = ""
 
     if request.method == "POST":
-
         title = request.form["title"]
         content = request.form["content"]
 
-        predictions = predict_tags(title + " " + content, top_k=10)
+        predictions = predict_tags(
+            title + " " + content,
+            top_k=10
+        )
 
     return render_template(
         "index.html",
@@ -24,5 +28,12 @@ def home():
         content=content
     )
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
